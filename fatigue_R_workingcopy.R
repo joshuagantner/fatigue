6665577b127a12b6570051eb4abe90daebb986d2
Joshua Gantner <josh.gantner@gmail.com>
2023-06-03 13:46:44 +0200

    9.2.1

--- fatigue_R_workingcopy.R
+++ fatigue_R_workingcopy.R
@@ -1,30 +1,52 @@
-# set working directory, load packages and toggle verbosity
+# Peripheral fatigues detrimental effect on motor training - underlying mechanisms
+# Author: Joshua Gantner
+# eMail:  josh.gantner@gmail.com
+
+# 0. SETUP
+# set working directory
 getwd()
 dir()
 rm(list = ls())
 setwd('/Users/joshuagantner/Library/CloudStorage/OneDrive-UniversitätZürichUZH/Files/Studium/Masterarbeit/0 v9')
+# load packages
 library(lme4)
 library(robustlmm)
+# toggle verbosity
 v_toggle = 0;
 
-# read in data from table
+# 1. LOAD DATA
+# read in data from csv table, as created by matlab data processing script
 D <- read.csv("table_space5.csv")
+
+# 2. PREPROCESS
+# mark categorical variables
 D$group <- as.factor(D$group)
 D$day <- as.factor(D$day)
 D$identifier <- as.factor(D$identifier)
-
-# ad a continuous variable for trials completed within a day. this serves as indicator for time passing/progression of training.
+# ad a continuous variable for trials completed within a day 
+# ... to serves as indicator for time passing/progression of training
 D$training <- (D$block-1)*30+D$trial
 D$training <- D$training*(1/120)*4 # scaled to 1 session = +1, thereby a day with 4 sessions of 30 trials starts at 1/30 and ends at 4
 
+# 3. FIT TRAINING-DAY-MODEL
+Dd1 <- subset(D,D$day==1)
+modelD1 <- rlmer(distance~group*training+(1+training|identifier), data=Dd1, verbose=v_toggle)
+summary(modelD1)
+# determine the satterthwaite approximation of degrees of freedom
+m <- lmer(distance~group*training+(1+training|identifier), data=Dd1)
+dfs <- data.frame(coef(summary(m)))$df
+# calculate p-values for the fixed effects of the robust model
+coefs <- coef(summary(modelD1))
+pvaluesD1 <- 2*pt(abs(coefs[,3]), dfs, lower=FALSE)
+pvaluesD1
+
+# 4. FIT 2-DAY-MODEL
 # fit the full robust model determining the effect of group*day*training on distance
 modelFull <- rlmer(distance~group*day*training+(1+training|identifier), data=D, verbose=v_toggle)
 summary(modelFull)
-
 # determine the satterthwaite approximation of degrees of freedom
 m <- lmer(distance~group*day*training+(1+training|identifier), data=D)
 dfs <- data.frame(coef(summary(m)))$df
-
 # calculate p-values for the fixed effects of the robust model
 coefs <- coef(summary(modelFull))
 p.values <- 2*pt(abs(coefs[,3]), dfs, lower=FALSE)
