daa393a1c71c48df816d0feb4acb02da415bd28e
Joshua Gantner <josh.gantner@gmail.com>
2023-06-08 10:56:07 +0200

    9.2.9 completed
    • added fatigue quotient to statistics
    • introduced R markdown

--- fatigue_R_statistics.R
+++ fatigue_R_statistics.R
@@ -1,13 +1,15 @@
-# Peripheral fatigues detrimental effect on motor training - underlying mechanisms
-# Author: Joshua Gantner
-# eMail:  josh.gantner@gmail.com
-
+---
+project:  Peripheral fatigues detrimental effect on motor training - underlying mechanisms
+script: statistics
+Author: Joshua Gantner
+eMail:  josh.gantner@gmail.com 
+---
 
 
 # 0. SETUP
-# set working directory
+<!-- set working directory -->
 setwd('/Users/joshuagantner/Library/CloudStorage/OneDrive-UniversitätZürichUZH/Files/Studium/Masterarbeit/0 v9')
-# load packages
+<!-- load packages -->
 library(lmerTest)
 library(robustlmm)
 library(MuMIn)
@@ -15,55 +17,55 @@ library(dplyr)
 v_toggle = 0 # toggle verbosity
 
 
-
-# 1. LOAD VARIABILITY DATA
-# read in data from csv table, as created by matlab data processing script
+# 1. MODEL VARIABILITY
+## load data
+<!-- read in data from csv table, as created by matlab data processing script -->
 D <- read.csv("table_space5.csv")
 
-# 2. PREPROCESS
-# mark categorical variables
+## preprocess
+<!-- mark categorical variables -->
 D$group <- as.factor(D$group)
 D$identifier <- as.factor(D$identifier)
-# ad a continuous variable for trials completed within a day 
-# ... to serves as indicator for time passing/progression of training
+<!-- ad a continuous variable for trials completed within a day to serves as indicator for time passing/progression of training -->
 D$training <- (D$block-1)*30+D$trial
 D$training <- D$training*(1/120)*1 # scale to 1 day = +1
-#D <- subset(D,D$distance<quantile(D$distance, c(.94)))
+ <!--D <- subset(D,D$distance<quantile(D$distance, c(.94))) -->
 
 
-# 3. FIT 2-DAY-MODEL
+## fit models
+### 2-day
 modelFull <- rlmer(distance~group*day*training+(1+training|identifier), data=D, verbose=v_toggle)
-# determine the satterthwaite approximation of degrees of freedom
+<!-- satterthwaite approximation of degrees of freedom -->
 m <- lmer(distance~group*day*training+(1+training|identifier), data=D)
 dfs <- data.frame(coef(summary(m)))$df
-# calculate p-values for the fixed effects of the robust model
+<!-- p-values for fixed effects -->
 coefs <- coef(summary(modelFull))
 pvalues <- 2*pt(abs(coefs[,3]), dfs, lower=FALSE)
 tableFull <- cbind(coefs,data.frame(pvalues))
 tableFull
 
 
-# 4. FIT TRAINING DAY FATIGUED COLLECTIVE MODEL
+### trainig day - fatigued collective
 Dd1 <- subset(D,D$day==1)
 Dd1 <- Dd1 %>% mutate(fatigued = ifelse(group == 1, 0, 1))
 modelD1 <- rlmer(distance~fatigued*training+(1|identifier), data=Dd1, verbose=v_toggle)
-# determine the satterthwaite approximation of degrees of freedom
+<!-- satterthwaite approximation of degrees of freedom -->
 m <- lmer(distance~fatigued*training+(1|identifier), data=Dd1)
 dfs <- data.frame(coef(summary(m)))$df
-# calculate p-values for the fixed effects of the robust model
+<!-- p-values for the fixed -->
 coefs <- coef(summary(modelD1))
 pvalues <- 2*pt(abs(coefs[,3]), dfs, lower=FALSE)
 tableD1 <- cbind(coefs,data.frame(pvalues))
 tableD1
 
 
-# 5. FIT CONTROL DAY MODEL
+### control day
 Dd2 <- subset(D,D$day==2)
 modelD2 <- rlmer(distance~group*training+(1|identifier), data=Dd2, verbose=v_toggle)
-# determine the satterthwaite approximation of degrees of freedom
+<!-- satterthwaite approximation of degrees of freedom -->
 m <- lmer(distance~group*training+(1|identifier), data=Dd2)
 dfs <- data.frame(coef(summary(m)))$df
-# calculate p-values for the fixed effects of the robust model
+<!-- p-values for the fixed effects -->
 coefs <- coef(summary(modelD2))
 pvalues <- 2*pt(abs(coefs[,3]), dfs, lower=FALSE)
 tableD2 <- cbind(coefs,data.frame(pvalues))
@@ -71,38 +73,40 @@ tableD2
 
 
 
-# 6. MODEL SKILL DATA
-# read in data from csv table, as created by matlab data processing script
+# 2. MODEL SKILL
+## load data
+<!-- read in data from csv table, as created by matlab data processing script -->
 Dskill <- read.csv("table_skill.csv")
-# mark categorical variables
+<!-- mark categorical variables-->
 Dskill$group <- as.factor(Dskill$group)
 Dskill$ID <- as.factor(Dskill$ID)
 
-# fit 2-day-model
+## fot models
+### 2-day
 modelSkill <- lmer(skillp~group*day*BN+(1|ID), data=Dskill)
 tableSkill <- coef(summary(modelSkill))
 tableSkill
 
-# fit training-day-model
+### training-day-model
 DskillD1 <- subset(Dskill,Dskill$day==1)
 modelSkillD1 <- lmer(skillp~group*BN+(1|ID), data=DskillD1)
 tableSkillD1 <- coef(summary(modelSkillD1))
 tableSkillD1
 
-# fit training-day-model fatigued collective
+### training day - fatigued collective
 DskillD1 <- subset(Dskill,Dskill$day==1)
 DskillD1 <- DskillD1 %>% mutate(fatigued = ifelse(group == 1, 0, 1))
 modelSkillD1f <- lmer(skillp~fatigued*BN+(1|ID), data=DskillD1)
 tableSkillD1f <- coef(summary(modelSkillD1f))
 tableSkillD1f
 
-# fit control-day-model
+### control day
 DskillD2 <- subset(Dskill,Dskill$day==2)
 modelSkillD2 <- lmer(skillp~group*BN+(1|ID), data=DskillD2)
 tableSkillD2 <- coef(summary(modelSkillD2))
 tableSkillD2
 
-# fit control-day-model with FSD (group2) as baseline
+### control day FSD baseline
 DskillD2 <- subset(Dskill,Dskill$day==2)
 DskillD2$group <- factor(DskillD2$group, levels = c(2, 1, 3))
 modelSkillD2 <- lmer(skillp~group*BN+(1|ID), data=DskillD2)
@@ -111,8 +115,8 @@ tableSkillD2
 
 
 
-# 7. CORRELATION OF VARIABILITY AND LEARNING
-# create combined variability-skill table
+# 3. CORRELATIONS
+## create combined variability-skill table
 Dvs <- Dskill
 Dvs$variability <- NA
 for (i in 1:nrow(Dvs)) {
@@ -125,7 +129,7 @@ for (i in 1:nrow(Dvs)) {
 }
 Dvs <- na.omit(Dvs)
 
-# correlations variability // skill
+## correlate variability // skill
 correlations <- data.frame(
   group = c(1,1,2,2,3,3),
   day = c(1,2,1,2,1,2),
@@ -144,8 +148,8 @@ correlations
 
 
 
-# 8. SHAPE OF VARIABILITY
-# fit kurtosis
+# 4. SHAPE of VARIABILITY
+## kurtosis
 DshapeK <- read.csv("table_kurtosis_space5.csv") # load data
 DshapeK$group <- as.factor(DshapeK$group) # mark categorical as factor
 DshapeK$identifier <- as.factor(DshapeK$identifier)
@@ -153,7 +157,7 @@ modelShapeK <- lmer(kurtosis~group + day*block+(1|identifier), data = DshapeK) #
 tableShapeK <- coef(summary(modelShapeK))
 tableShapeK
 
-# fit skew
+## skew
 DshapeS <- read.csv("table_skew_space5.csv") # load data
 DshapeS$group <- as.factor(DshapeS$group) # mark categorical as factor
 DshapeS$identifier <- as.factor(DshapeS$identifier)
@@ -161,7 +165,7 @@ modelShapeS <- lmer(skew~group + day*block+(1|identifier), data = DshapeS) # fit
 tableShapeS <- coef(summary(modelShapeS))
 tableShapeS
 
-# fit range
+## range
 DshapeR <- read.csv("table_range_space5.csv") # load data
 DshapeR$group <- as.factor(DshapeR$group) # mark categorical as factor
 DshapeR$identifier <- as.factor(DshapeR$identifier)
@@ -171,8 +175,8 @@ tableShapeR
 
 
 
-# 9. RELEVANT VARIABILITY FOR LEARNING
-# Create a vector of filenames
+# 5. DRIVERS of VARIABILITY CHANGE
+## link data
 filenames <- c(
   "table_spaceFDI.csv",
   "table_spaceAPB.csv",
@@ -180,12 +184,12 @@ filenames <- c(
   "table_spaceIntrinsic.csv", 
   "table_spaceExtrinsic.csv")
 
-# Create an empty data frame to store the results
+<!-- Create an empty data frame to store the results -->
 r2 <- data.frame(subspace = character(),
                  cR2 = numeric(),
                  stringsAsFactors = FALSE)
 
-# Loop through the filenames
+## find r2
 for (filename in filenames) {
   # Read the CSV file
   Dspace <- read.csv(filename)
@@ -205,19 +209,53 @@ r2
 
 
 
-# 10. MAXIMUM EMG AMPLITUDE
-# load data
+# 6. MAXIMUM EMG AMPLITUDE
+## load data
 Dmax <- read.csv("table_maxemgamp_FDI.csv")
 Dmax$group <- as.factor(Dmax$group)
 Dmax$time <- ((Dmax$block-1)*30+Dmax$trial)/30
 Dmax <- Dmax %>% mutate(fatigued = ifelse(group == 1, 0, 1))
-# simple Model robust
+
+## fit model
 modelMaxamp <- rlmer(max~fatigued*time + (1|identifier), data = Dmax[Dmax$day==1, ], verbose = v_toggle)
-# determine the satterthwaite approximation of degrees of freedom
+<!-- satterthwaite approximation of degrees of freedom -->
 m <- lmer(max~fatigued*time + (1|identifier), data = Dmax[Dmax$day==1, ])
 dfs <- data.frame(coef(summary(m)))$df
-# calculate p-values for the fixed effects of the robust model
+<!-- calculate p-values for the fixed effects of the robust model -->
 coefs <- coef(summary(modelMaxamp))
 pvalues <- 2*pt(abs(coefs[,3]), dfs, lower=FALSE)
 tableMaxamp <- cbind(coefs,data.frame(pvalues))
 tableMaxamp
+
+## calculate fatigue quotient
+Dtaskfail <- Dmax %>%
+  filter(block %in% 1:4) %>%
+  group_by(identifier, day, group, block) %>%
+  summarise(max_trial = max(trial)) %>%
+  pivot_wider(names_from = block, values_from = max_trial, names_prefix = "trial_") %>%
+  ungroup()
+pivot_df
+
+addedFatigue <- data.frame(
+  group = c(1,2,3),
+  added_fatigue = c(NaN,NaN,NaN)
+)
+
+### include missing session 4s
+for (group in 1:3) {
+  foo <- Dtaskfail[Dtaskfail$group==group & Dtaskfail$day==1, c('trial_1', 'trial_4')]
+  foo$trial_4[is.na(foo$trial_4)] <- 0
+  foo <- na.omit(foo)
+  q <- (foo$trial_1-foo$trial_4)/foo$trial_1
+  addedFatigue[addedFatigue$group==group, 'added_fatigue'] <- mean(q)
+}
+addedFatigue
+
+### exclude missing session 4s
+for (group in 1:3) {
+  foo <- Dtaskfail[Dtaskfail$group==group & Dtaskfail$day==1, c('trial_1', 'trial_4')]
+  foo <- na.omit(foo)
+  q <- (foo$trial_1-foo$trial_4)/foo$trial_1
+  addedFatigue[addedFatigue$group==group, 'added_fatigue'] <- mean(q)
+}
+addedFatigue
